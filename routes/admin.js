605369b8b12529f2256839407b333ea77e7c5022
NineOne91 <ihim512@gmail.com>
2021-03-14 14:15:31 +0700

    clean project

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const adminController = require('../controllers/adminController')
-
-router.get('/dashboard', adminController.viewDashboard);
-router.get('/aik', adminController.viewAik);
-
-module.exports = router;
